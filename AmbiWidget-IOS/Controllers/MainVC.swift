c452895845ea4e1dfb07c6dcbc312c3a27a78525
BrandonYuen <brandon97@hotmail.com>
2018-12-17 16:29:47 +0800

    Settings Page

--- AmbiWidget-IOS/Controllers/MainVC.swift
+++ AmbiWidget-IOS/Controllers/MainVC.swift
@@ -13,32 +13,23 @@ class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
 		
-		// Get the refresh token
+		// Add listener for when application is opened
+		NotificationCenter.default.addObserver(self, selector: #selector(viewDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+    }
+	
+	@objc func viewDidBecomeActive() {
+		// Check if refresh token is set
 		TokenManager.loadTokenFromUserDefaults(with: .RefreshToken)
-		.done { token in
-			// Do nothing...
-			
-			// Debugging
-			DeviceManager.API.getDeviceList()
-			.map { arrayOfDevices -> ([Device]) in
-				try DeviceManager.Local.saveDeviceList(deviceList: arrayOfDevices)
-				let deviceList = try DeviceManager.Local.getDeviceList()
-				return deviceList
-			}.done { deviceList in
-				print("getDeviceList: \(deviceList)")
-			}.catch { error in
-				print ("\(String(describing: self)) Error: \(error)")
-			}
-			
-		}.catch { error in
+		.catch { error in
 			// Show auth view page
 			let vc = self.storyboard?.instantiateViewController(withIdentifier: "auth") as! AuthVC
 			self.present(vc, animated: true, completion: nil)
 		}
+	}
+	
+	// Open Application Settings
+	@IBAction func settingsButton(_ sender: UIBarButtonItem) {
+		UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, completionHandler: nil)
 	}
 }

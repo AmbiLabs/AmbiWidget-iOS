0ee0ab5688eddd5165fcad7bedd749f5ea08bbed
Milan <msosef@yahoo.com>
2019-01-03 16:58:10 +0800

    Fix layout cropping in compact mode.

--- TodayExtension/Controllers/TodayViewController.swift
+++ TodayExtension/Controllers/TodayViewController.swift
@@ -11,7 +11,7 @@ import NotificationCenter
 
 // TODO:
 // 1) Fix deviceViewmodelIndex not saved after widgetPerformUpdate.
-// 2) Fix icons changing to white color.
+// 2) DONE Fix icons changing to white color.
 
 class TodayViewController: UIViewController, NCWidgetProviding {
     var deviceViewModels: [DeviceViewModel]?
@@ -28,6 +28,8 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var modeIcon: UIImageView!
     
+    @IBOutlet weak var buttonRow: UIStackView!
+    
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
@@ -40,8 +42,10 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
         if activeDisplayMode == .compact {
+            self.buttonRow.isHidden = true
             self.preferredContentSize = maxSize
         } else if activeDisplayMode == .expanded {
+            self.buttonRow.isHidden = false
             self.preferredContentSize = CGSize(width: maxSize.width, height: 220)
         }
     }
